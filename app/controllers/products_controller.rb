62f6e4ef8fe966e6199f1c194030aa425b6891bb
Melissa Gonzalez <marvelissa@gmail.com>
2017-07-05 21:04:46 -0500

    Add pages with all products and first product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def products_method
+    @products = Product.all
+    render "products.html.erb"
+  end
+
+  def unicorn_tears_method
+    @unicorn_tears = Product.find_by(id: 1)
+    render "unicorn_tears.html.erb"
+  end
 end
