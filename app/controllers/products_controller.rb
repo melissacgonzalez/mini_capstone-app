211a8a0ca131551a1410d48abfeebdf7892cf397
Melissa Gonzalez <marvelissa@gmail.com>
2017-07-10 19:36:15 -0500

    Add index, new, create, and show methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,22 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find_by(name: params["name"])
-    @name = @product.name
-    @price = @product.price
-    @description = @product.description
-    @image = @product.image
+    @product = Product.find_by(id: params["id"])
     render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      image: params[:image],
+      price: params[:price],
+      description: params[:description]
+      )
+    @product.save
+    render "create.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "products#index"
   get "/products" => "products#index"
-  get "/products/:name" => "products#show"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+  get "/products/:id" => "products#show"
 end
