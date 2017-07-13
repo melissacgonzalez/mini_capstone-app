c68f083aa767eb9ba5e75521ede8fab8dd675282
Melissa Gonzalez <marvelissa@gmail.com>
2017-07-12 21:02:13 -0500

    Add tax & total to Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ApplicationRecord
+  def sale_method
+    if price < 2
+      message = "Discount Item!"
+    else
+      message = "Everyday Value!"
+    end
+  end
+
+  def discounted?
+    if sale_method == "Discount Item!"
+      @color = "red"
+    else
+      @color = "default"
+    end
+  end
+
+  def tax
+    price * 0.09 
+  end
+
+  def total
+    price + tax
+  end
 end
